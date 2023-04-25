cbead58132ecad6be3acc6f13ea310dc4e454e08
Jose Alvarez <alv19392@uvg.edu.gt>
2023-04-25 09:38:54 -0600

    Pruebas iniciales
    Primeras pruebas, realizadas para comprobar las conexiones fisicas de los modulos

---
+++ CSEnvio.py
@@ -0,0 +1,24 @@
+import json
+import serial
+import random
+
+if __name__=="__main__":
+    print("Ready ...")
+    ser = serial.Serial('COM3',baudrate=115200)
+
+    if ser.isOpen():
+        
+        try:
+            mensaje = 'Prueba\n'
+            ser.write(mensaje.encode("utf-8"))
+            ser.flush()
+            echo = ser.readline().decode("utf-8")  
+            print(echo);  
+
+        except Exception as e:
+            print(e)
+
+        ser.close()
+
+    else:
+        print("No se pudo abrir el puerto")
